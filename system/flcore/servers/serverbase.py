580f66c9d1330a6591b0a90b7048ee2b46f84d68
BobCheng <42957089+R-16Bob@users.noreply.github.com>
2024-07-11 00:06:50 +0800

    Restore branch (#3)
    * resort: my-fork
    
    * merge and solve conflicts

--- system/flcore/servers/serverbase.py
+++ system/flcore/servers/serverbase.py
@@ -198,6 +198,7 @@ class Server(object):
                 hf.create_dataset('rs_test_acc', data=self.rs_test_acc)
                 hf.create_dataset('rs_test_auc', data=self.rs_test_auc)
                 hf.create_dataset('rs_train_loss', data=self.rs_train_loss)
+                hf.create_dataset('rs_cost_time', data=self.Budget)
 
     def save_item(self, item, item_name):
         if not os.path.exists(self.save_folder_name):
