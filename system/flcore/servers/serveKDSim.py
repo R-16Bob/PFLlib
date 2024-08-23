85b2a43ec4ebfd1d950d136613ba577dc4a5241e
R-16Bob <15343443062@163.com>
2024-08-23 19:52:24 +0800

    Debug: FedKDSim now can run

--- system/flcore/servers/serveKDSim.py
+++ system/flcore/servers/serveKDSim.py
@@ -1,6 +1,7 @@
 # PFLlib: Personalized Federated Learning Algorithm Library
 # Copyright (C) 2021  Jianqing Zhang
-
+import copy
+import random
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -46,13 +47,15 @@ class FedKDSim(Server):
         # Fixed gaussian noise for model embedding
         if "MNIST" in self.dataset:
             mean, std = 0, 1
-            self.rgauss = torch.randn(1, 28, 28) * std + mean
+            self.rgauss = torch.randn(1, 1, 28, 28) * std + mean
+            # print('rgauss:', self.rgauss.shape)
         elif "Cifar10" in args.dataset:
             mean, std = 0.0, 0.1
             batch_size = 1
             self.rgauss = torch.randn(batch_size, 3, 32, 32) * std + mean
         else:
             raise NotImplementedError("Dataset gaussian noise not implemented!")
+        self.rgauss=self.rgauss.to(self.device)
 
     # override send_models
     def send_models(self,i):
@@ -60,10 +63,10 @@ class FedKDSim(Server):
             super().send_models()
         else:
             assert (len(self.selected_clients) > 0)
-
+            # print(self.personalized_model)
             for sid,client in enumerate(self.selected_clients):
                 start_time = time.time()
-
+                # print("sid:",sid)
                 client.set_parameters(self.personalized_model[sid])  # personalized model
 
                 client.send_time_cost['num_rounds'] += 1
@@ -72,13 +75,13 @@ class FedKDSim(Server):
     # Construct a KD-Tree after clients finished training
     def client_similarity(self):
         for client in self.selected_clients:
-            self.cid_to_vectors[client.id] = np.squeeze(client.model(self.rgauss))  # embedding models dictionary
+            self.cid_to_vectors[client.id] = np.squeeze(client.model(self.rgauss).detach().cpu())  # embedding models dictionary
         self.vid_to_cid = list(self.cid_to_vectors.keys())
-        self.vectors = list(self.cid_to_vectors.values())
+        self.vectors = np.vstack(list(self.cid_to_vectors.values()))
         self.tree = spatial.KDTree(self.vectors)
 
     # Search similar models for each selected client using KD-Tree
-    def get_similar_models(self,i):
+    def get_similar_models(self, epoch):
         #if cid in self.cid_to_vectors and (self.curr_round+1)%self.args.h_interval == 0:
         for client in self.selected_clients:
             cid=client.id
@@ -87,21 +90,21 @@ class FedKDSim(Server):
             self.sims[cid]=[]
             for vid in searchs[1]:
                 self.sims[cid].append(self.vid_to_cid[vid])
-            print("Epoch:{},cid:{},sims:{}".format(i, cid, self.sims[cid]))
+            # print("Epoch:{},cid:{},sims:{}".format(epoch, cid, self.sims[cid]))
 
     # override receive_models
-    def receive_models(self):
+    def receive_models(self,epoch):
         assert (len(self.selected_clients) > 0)
 
-        active_clients = random.sample(
-            self.selected_clients, int((1-self.client_drop_rate) * self.current_num_join_clients))  # not used in KDSim
+        # active_clients = random.sample(
+        #     self.selected_clients, int((1-self.client_drop_rate) * self.current_num_join_clients))  # not used in KDSim
 
-        self.uploaded_ids = {}
+        # self.uploaded_ids = {}
         self.uploaded_weights = {}
         self.uploaded_models = {}
         tot_samples = [0 for i in range(self.current_num_join_clients)]
 
-        for sid,client in enumerate(active_clients):
+        for sid,client in enumerate(self.selected_clients):
             try:
                 client_time_cost = client.train_time_cost['total_cost'] / client.train_time_cost['num_rounds'] + \
                         client.send_time_cost['total_cost'] / client.send_time_cost['num_rounds']
@@ -109,30 +112,31 @@ class FedKDSim(Server):
                 client_time_cost = 0
             if client_time_cost <= self.time_threthold:
                 # get weights for each selected client
-                tot_samples[sid] += client.train_samples
                 agg_list=self.sims[client.id]
-                self.uploaded_ids[sid] = agg_list
+                # self.uploaded_ids[sid] = agg_list
                 self.uploaded_weights[sid] = []
                 self.uploaded_models[sid] = []
                 for agg_cid in agg_list:
                     agg_client=self.clients[agg_cid]
+                    tot_samples[sid] += agg_client.train_samples
                     self.uploaded_weights[sid].append(agg_client.train_samples)
                     self.uploaded_models[sid].append(agg_client.model)
                 for i, w in enumerate(self.uploaded_weights[sid]):
-                    self.uploaded_weights[i] = w / tot_samples[sid]
+                    self.uploaded_weights[sid][i] = w / tot_samples[sid]
+                print("Epoch:{},cid:{},agg_cid:{},w:{}".format(epoch, client.id, agg_list, self.uploaded_weights[sid]))
 
     # Override aggregate_parameters
     def aggregate_parameters(self):
+        self.personalized_model = {}
         for sid,client in enumerate(self.selected_clients):
             assert (len(self.uploaded_models[sid]) > 0)
-            self.personalized_model={}
             self.personalized_model[sid] = copy.deepcopy(self.uploaded_models[sid][0])
             for param in self.personalized_model[sid].parameters():
                 param.data.zero_()
-
+            # print(self.uploaded_weights[sid])
             for w, client_model in zip(self.uploaded_weights[sid], self.uploaded_models[sid]):
                 self.add_parameters(w, client_model,sid)
-
+        # print("pmodle:",self.personalized_model.keys())
     # override add_parameters
     def add_parameters(self, w, client_model, sid):
         for personalized_param, client_param in zip(self.personalized_model[sid].parameters(), client_model.parameters()):
@@ -140,8 +144,9 @@ class FedKDSim(Server):
     def train(self):
         for i in range(self.global_rounds+1):  # global round
             s_t = time.time()
-            self.selected_clients = self.select_clients()
             self.send_models(i)  # override send models
+            self.selected_clients = self.select_clients()
+
 
             if i%self.eval_gap == 0:
                 print(f"\n-------------Round number: {i}-------------")
@@ -157,12 +162,12 @@ class FedKDSim(Server):
             # [t.join() for t in threads]
 
             # Construct a KD-Tree after clients finished training
-            self.client_similarity(i)
+            self.client_similarity()
 
             # Search similar models using KD-Tree
-            self.get_similar_models()
+            self.get_similar_models(i)
             # personalized aggregation for selected clients
-            self.receive_models()
+            self.receive_models(i)
             self.aggregate_parameters()
 
             self.Budget.append(time.time() - s_t)
